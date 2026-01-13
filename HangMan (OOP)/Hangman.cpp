e7d8a59899ce0a04619a515ea60bed0ee595def0
Jovic <jasonmilakovic@outlook.com>
2026-01-13 16:37:44 +0000

    Add files via upload

--- HangMan (OOP)/Hangman.cpp
+++ HangMan (OOP)/Hangman.cpp
@@ -1,1 +1,20 @@
 #include "Hangman.h"
+#include <fstream>
+#include <string>
+
+Hangman::Hangman() :  m_answer(""), m_guessProgress(), m_playerInput()
+{
+
+}
+
+void Hangman::GenerateAnswer()
+{
+	int m_index;
+	std::string m_answerList; 
+	std::fstream answerFile("AnswerList.txt");
+
+	while (getline(answerFile, m_answerList[]))
+	{
+
+	}
+}
--- HangMan (OOP)/Hangman.h
+++ HangMan (OOP)/Hangman.h
@@ -1,10 +1,19 @@
 #pragma once
+#include <iostream>
+
+
 class Hangman
 {
 public:
+	
+	Hangman();
+
+	void GenerateAnswer();
+
 
 private:
-	char m_answer;
+	
+	std::string m_answer;
 	char* m_playerInput;
 	char m_guessProgress;
 	char m_hangManFull[GRID_SIZE][GRID_SIZE] =
@@ -17,7 +26,18 @@ private:
 		'|', '~', '~', '~', 'X', '~', '~',
 		'|', '_', '_', '/', '~', '\\', '~'
 	};
-	char m_hangManEmpty[GRID_SIZE][GRID_SIZE];
+	char m_hangManEmpty[GRID_SIZE][GRID_SIZE] =
+	{
+		'~', '~', '~', '~', '~', '~', '~',
+		'~', '~', '~', '~', '~', '~', '~',
+		'~', '~', '~', '~', '~', '~', '~',
+		'~', '~', '~', '~', '~', '~', '~',
+		'~', '~', '~', '~', '~', '~', '~',
+		'~', '~', '~', '~', '~', '~', '~',
+		'~', '~', '~', '~', '~', '~', '~',
+	};
+	
+
 };
 
 const int GRID_SIZE = 7;
